7461566caa742c969bad7c1ed9abf71e8da4118b
eronryabets <eronryabets@gmail.com>
2024-09-12 17:57:21 +0300

    Pagination buttons style.

--- src/components/PaginationControls/PaginationControls.tsx
+++ src/components/PaginationControls/PaginationControls.tsx
@@ -1,5 +1,6 @@
 import styles from './PaginationControls.module.scss';
 import React from "react";
+import {Button} from "react-bootstrap";
 
 interface PaginationControlsProps {
     currentPage: number;
@@ -8,25 +9,31 @@ interface PaginationControlsProps {
 }
 
 export const PaginationControls = ({
-    currentPage,
-    setCurrentPage,
-    nextPageUrl,
-}: PaginationControlsProps) => {
+                                       currentPage,
+                                       setCurrentPage,
+                                       nextPageUrl,
+                                   }: PaginationControlsProps) => {
     return (
-        <div className="pagination-controls">
-            <button
-                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                disabled={currentPage === 1}
-            >
-                Previous
-            </button>
-            <span>Page {currentPage}</span>
-            <button
-                onClick={() => setCurrentPage((prev) => prev + 1)}
-                disabled={!nextPageUrl}
-            >
-                Next
-            </button>
+        <div className={styles.paginationControls}>
+            <div className={styles.container}>
+                <Button
+                    className={styles.button}
+                    variant="primary"
+                    onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                    disabled={currentPage === 1}
+                >
+                    Previous
+                </Button>
+                <span className={`mx-3 ${styles.textContent}`}>Page {currentPage}</span>
+                <Button
+                    className={styles.button}
+                    variant="primary"
+                    onClick={() => setCurrentPage((prev) => prev + 1)}
+                    disabled={!nextPageUrl}
+                >
+                    Next
+                </Button>
+            </div>
         </div>
     );
 };
