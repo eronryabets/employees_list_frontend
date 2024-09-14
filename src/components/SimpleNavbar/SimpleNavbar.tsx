ffc40bbc0ac668c0309f2e34c24cf588985e4896
eronryabets <eronryabets@gmail.com>
2024-09-14 18:51:00 +0300

    Illumination of active buttons on the navbar

--- src/components/SimpleNavbar/SimpleNavbar.tsx
+++ src/components/SimpleNavbar/SimpleNavbar.tsx
@@ -5,36 +5,48 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import classNames from 'classnames';
 import {ThemeSwitcher} from "../ThemeSwitcher";
-import { Link } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 
 export const SimpleNavbar = () => {
+
+    const location = useLocation(); // Получаем текущий путь
+
     return (
         <div className={styles.simpleNavbar}>
             <Navbar expand="lg" className={classNames('bg-body-tertiary', styles.main)}>
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Employees</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Brand as={Link} to="/" >Employees</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/add">Add</Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/"
+                                className={classNames({ [styles.active]: location.pathname === '/' })}
+                            >
+                                Emp. List
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/add"
+                                className={classNames({ [styles.active]: location.pathname === '/add' })}
+                            >
+                                Add
+                            </Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider/>
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
-                    <ThemeSwitcher/>
+                    <ThemeSwitcher />
                 </Container>
             </Navbar>
-             </div>
+        </div>
     );
 };
