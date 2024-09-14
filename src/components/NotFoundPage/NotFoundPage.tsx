a202ef307944a50365dab1af5ec3340d0e761f72
eronryabets <eronryabets@gmail.com>
2024-09-14 21:26:52 +0300

    404 animations

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,38 @@
 import styles from './NotFoundPage.module.scss';
 import {Link} from "react-router-dom";
+import {useEffect} from "react";
+import anime from 'animejs/lib/anime.es.js';
+import {NotFoundAnimations} from "./NotFoundAnimations";  // Импорт anime.js
 
-interface NotFoundPageProps { }
 
 export const NotFoundPage = () => {
+
+        useEffect(() => {
+        // Анимация для svg
+        anime({
+            targets: '.row svg',
+            translateY: 10,
+            autoplay: true,
+            loop: true,
+            easing: 'easeInOutSine',
+            direction: 'alternate'
+        });
+
+        // Анимация для элемента с id "zero"
+        anime({
+            targets: '#zero',
+            translateX: 10,
+            autoplay: true,
+            loop: true,
+            easing: 'easeInOutSine',
+            direction: 'alternate',
+            // scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+            // rotateY: { value: '+=180', delay: 200 },
+        });
+    }, []); // useEffect чтобы анимация запускалась только при монтировании компонента
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
+        <div className={styles.NotFoundPage}>
+           <NotFoundAnimations/>
             <Link to="/">Go Back to Home</Link>
         </div>
     );
